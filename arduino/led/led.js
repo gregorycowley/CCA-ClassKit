449642780b1d0804b879b05aa47a61f0399889fe
Gregory Cowley <gregory@cowleystudios.com>
2020-07-19 10:18:04 -0700

    Adding LED control

---
+++ arduino/led/led.js
@@ -0,0 +1,64 @@
+(function() {
+  'use strict';
+
+  document.addEventListener('DOMContentLoaded', event => {
+    let connectButton = document.querySelector("#connect");
+    let statusDisplay = document.querySelector('#status');
+    let LED = document.querySelector('#LED');
+
+    function connect() {
+      port.connect().then(() => {
+        statusDisplay.textContent = '';
+        connectButton.textContent = 'Disconnect';
+
+        port.onReceive = data => {
+          let textDecoder = new TextDecoder();
+          console.log(textDecoder.decode(data));
+        }
+        port.onReceiveError = error => {
+          console.error(error);
+        };
+      }, error => {
+        statusDisplay.textContent = error;
+      });
+    }
+
+    function onUpdate() {
+      if (!port) {
+        return;
+      }
+
+      let view = new Uint8Array(3);
+      view[0] = parseInt(LED.value);
+      port.send(view);
+    };
+
+    LED.addEventListener('input', onUpdate);
+
+    connectButton.addEventListener('click', function() {
+      if (port) {
+        port.disconnect();
+        connectButton.textContent = 'Connect';
+        statusDisplay.textContent = '';
+        port = null;
+      } else {
+        serial.requestPort().then(selectedPort => {
+          port = selectedPort;
+          connect();
+        }).catch(error => {
+          statusDisplay.textContent = error;
+        });
+      }
+    });
+
+    serial.getPorts().then(ports => {
+      if (ports.length == 0) {
+        statusDisplay.textContent = 'No device found.';
+      } else {
+        statusDisplay.textContent = 'Connecting...';
+        port = ports[0];
+        connect();
+      }
+    });
+  });
+})();
---
+++ arduino/led/sketch/sketch.ino
@@ -0,0 +1,40 @@
+#include <WebUSB.h>
+
+/**
+ * Creating an instance of WebUSBSerial will add an additional USB interface to
+ * the device that is marked as vendor-specific (rather than USB CDC-ACM) and
+ * is therefore accessible to the browser.
+ *
+ * The URL here provides a hint to the browser about what page the user should
+ * navigate to to interact with the device.
+ */
+WebUSB WebUSBSerial(1 /* https:// */, "gregorycowley.github.io/CCA-ClassKit/arduino/console/");
+
+#define Serial WebUSBSerial
+
+int led = 7;
+int del = 1000;
+int state = 0;
+
+void setup() {
+  while (!Serial) {
+    ;
+  }
+  Serial.begin(9600);
+  Serial.write("Sketch begins.\r\n");
+  Serial.flush();
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  if (Serial && Serial.available()) {
+    state = Serial.read();
+    if (state == 1) {
+      digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+      // delay(del);                       // wait for a second
+    } else {
+      digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+      // delay(del);      
+    }
+  }
+}
