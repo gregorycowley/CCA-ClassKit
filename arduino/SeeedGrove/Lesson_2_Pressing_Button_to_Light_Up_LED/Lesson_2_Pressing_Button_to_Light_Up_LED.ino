e78d264c84e2d3fb77447cc06dd5ea6c0e9bd55c
Gregory Cowley <gregory@cca.edu>
2020-11-17 10:00:27 -0800

    Adding more arduino content

--- arduino/SeeedGrove/Lesson_2_Pressing_Button_to_Light_Up_LED/Lesson_2_Pressing_Button_to_Light_Up_LED.ino
+++ arduino/SeeedGrove/Lesson_2_Pressing_Button_to_Light_Up_LED/Lesson_2_Pressing_Button_to_Light_Up_LED.ino
@@ -1,7 +1,9 @@
 //Button to turn ON/OFF LED
 //Constants won't change. They're used here to set pin numbers:
 
-const int buttonPin = 6; const int ledPin = 4;
+const int buttonPin = 6; 
+
+const int ledPin = 4;
 
 // variables will change:
 int buttonState = 0; 
@@ -9,22 +11,45 @@ int buttonState = 0;
 // the number of the pushbutton pin
 // the number of the LED pin
 // variable for reading the pushbutton statu
+
+
+
 void setup() {
+  
+  
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
+  
+  
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+
+  
 }
 
+
+
+
+
 void loop() {
-// read the state of the pushbutton value: 
-buttonState = digitalRead(buttonPin);
-// check if the pushbutton is pressed. If it is, the buttonState is HIG H:
-if (buttonState == HIGH) {
-// turn LED on: 
-digitalWrite(ledPin, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-} 
+  
+  // read the state of the pushbutton value: 
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2 = digitalRead(buttonPin2);
+  buttonState3 = digitalRead(buttonPin3);
+  buttonState4 = digitalRead(buttonPin4);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIG H:
+  
+  if (buttonState == HIGH) {
+    
+      // turn LED on: 
+      digitalWrite(ledPin, HIGH);
+    
+   } else {
+    
+      // turn LED off:
+      digitalWrite(ledPin, LOW);
+      
+  } 
 }
